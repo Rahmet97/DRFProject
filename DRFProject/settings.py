e567b130570396f5af2968ca41ecc8d18b11323d
Rahmet97 <ruslanovrahmet@gmail.com>
2022-09-13 19:52:22 +0500

    JWTAuthentication done

--- DRFProject/settings.py
+++ DRFProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
--- main/views.py
+++ main/views.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import UserSerializer
 
 
 @api_view(['GET'])
+@authentication_classes([JWTAuthentication])
 def getUser(request):
     try:
-        users = User.objects.filter(is_superuser=False)
-        users_json = UserSerializer(users.data, many=True)
+        users = request.user
+        users_json = UserSerializer(users)
         data = {
-            'data': users_json
+            'data': users_json.data
         }
     except Exception as e:
         data = {
